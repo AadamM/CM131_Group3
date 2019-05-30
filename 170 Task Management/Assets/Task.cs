a8437ebd0b798ead4ab283a3fd302685fcc78f3c
AadamM <armohamm@ucsc.edu>
2019-05-29 18:57:24 -0700

    Wrote the top label descriptions

---
+++ 170 Task Management/Assets/Task.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task : MonoBehaviour {
+
+    private string taskName;
+    private Member taskMember;
+    private int priority;
+    private float timeEstimate;
+    private enum progressState { assigned, inProgress, done };
+    private progressState currentProgress;
+
+    public void AddTask (string refTaskName, Member refTaskMember, int refPriority, float refTimeEstimate){
+        taskName = refTaskName;
+        taskMember = refTaskMember;
+        priority = refPriority;
+        timeEstimate = refTimeEstimate;
+
+        currentProgress = progressState.assigned;
+    }
+}
